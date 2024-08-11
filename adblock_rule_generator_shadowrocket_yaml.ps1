45c18422aa65efeeece14c3ac20d77aa8d3f178e
REIJI007 <miyajimareji007@gmail.com>
2024-08-11 16:20:32 +0800

    Update adblock_rule_generator_shadowrocket_yaml.ps1
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- adblock_rule_generator_shadowrocket_yaml.ps1
+++ adblock_rule_generator_shadowrocket_yaml.ps1
@@ -1,6 +1,6 @@
-# Title: AdBlock_Rule_For_Shadowrocket_RULESET
-# Description: 适用于Shadowrocket的域名拦截yaml格式RULE-SET，每20分钟更新一次，确保即时同步上游减少误杀
-# Homepage: https://github.com/REIJI007/AdBlock_Rule_For_Shadowrocket
+# Title: AdBlock_Rule_For_Quantumult_RULESET
+# Description: 适用于Quantumult的域名拦截yaml格式RULE-SET，每20分钟更新一次，确保即时同步上游减少误杀
+# Homepage: https://github.com/REIJI007/AdBlock_Rule_For_Quantumult
 
 # 定义广告过滤器URL列表
 $urlList = @(
@@ -75,28 +75,20 @@ foreach ($url in $urlList) {
 }
 
 # 对规则进行排序并添加DOMAIN,前缀
-$formattedRules = $uniqueRules | Sort-Object | ForEach-Object {"DOMAIN,$_"}
+$formattedRules = $uniqueRules | Sort-Object | ForEach-Object {"  - DOMAIN,$_"}
 
 # 统计生成的规则条目数量
 $ruleCount = $uniqueRules.Count
 
-# 创建文本格式的字符串
-$textContent = @"
-# Title: AdBlock_Rule_For_Shadowrocket_RULESET
-# Description: 适用于Shadowrocket的域名拦截yaml格式RULE-SET，每20分钟更新一次，确保即时同步上游减少误杀
-# Homepage: https://github.com/REIJI007/AdBlock_Rule_For_Shadowrocket
-# LICENSE1：https://github.com/REIJI007/AdBlock_Rule_For_Shadowrocket/blob/main/LICENSE-GPL3.0
-# LICENSE2：https://github.com/REIJI007/AdBlock_Rule_For_Shadowrocket/blob/main/LICENSE-CC%20BY-NC-SA%204.0
-
-# Generated AdBlock rules
-# Total entries: $ruleCount
-
+# 创建YAML格式的字符串
+$yamlContent = @"
+payload:
 $($formattedRules -join "`n")
 "@
 
 # 定义输出文件路径
-$outputPath = "$PSScriptRoot/adblock_reject_shadowrocket_ruleset.yaml"
-$textContent | Out-File -FilePath $outputPath -Encoding utf8
+$outputPath = "$PSScriptRoot/adblock_reject_quantumult_ruleset.yaml"
+$yamlContent | Out-File -FilePath $outputPath -Encoding utf8
 
 # 输出生成的有效规则总数
 Write-Host "生成的有效规则总数: $ruleCount"
